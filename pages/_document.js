995c1cf4e2cedcb844b3e6dc34936b4d5dcd21f4
Jonathan Shapiro <shapiroj18@gmail.com>
2021-12-07 22:10:35 -0500

    build with counter.dev didnt work

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <script>if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"shapiroj18",utcoffset:"-5"}))};sessionStorage.setItem("_swa","1");</script>  
           <link rel="icon" href="/images/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css?family=Montserrat&display=optional"
